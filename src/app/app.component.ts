8036d004ff986e97871f471daa6eac47b367c12e
SaraMarve <amfm5796@gmail.com>
2025-05-25 18:45:42 +0300

    הצגת מערך סטודנטים ומשתנה על המסך

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { StudentsListComponent } from './students-list/students-list.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
-}
+    title = 'האתר שלי';
+  
+    getGreeting(): string {
+      const hour = new Date().getHours();
+  
+      if (hour < 12) {
+        return 'בוקר טוב';
+      } else if (hour < 18) {
+        return 'צהריים טובים';
+      } else {
+        return 'ערב טוב';
+      }
+    }
+  }
---
+++ src/app/students-list/students-list.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+export class Students {
+      public id?: number;
+      public firstName?: string;
+      public lastName?:  string;
+      public address?: string;
+      public phone? : string;
+      public active?: boolean;
+      public average?: number;
+  }
+@Component({
+  selector: 'app-students-list',
+  templateUrl: './students-list.component.html',
+  styleUrls: ['./students-list.component.css']
+})
+
+export class StudentsListComponent {
+  students = [
+    {
+      id: 1,
+      firstName: 'שרה',
+      lastName: 'כהן',
+      address: 'תאנה',
+      phone: '050-1234567',
+      active: true,
+      average: 92
+    },
+    {
+      id: 2,
+      firstName: 'חני',
+      lastName: 'לוי',
+      address: 'חיטה',
+      phone: '052-7654321',
+      active: false,
+      average: 81
+    },
+    {
+      id: 3,
+      firstName: 'רבקה',
+      lastName: 'ישראלי',
+      address: 'רימון',
+      phone: '053-9876543',
+      active: true,
+      average: 88
+    }
+  ];
+
+
+}
